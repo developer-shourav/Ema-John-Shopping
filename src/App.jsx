b2b9c3c25615cc429d917476d4b7eb67a31d131e
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-27 23:49:20 +0600

    shop page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Header from './components/Header/Header'
+import Shop from './components/Shop/Shop'
 
 function App() {
 
   return (
     <div className="App">
       <Header> </Header>
+      <Shop> </Shop>
     </div>
   )
 }
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+             <h2>Product is coming here.</h2>
+            </div>
+
+            <div className="cart-container">
+              <h4>Order Summery</h4>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
