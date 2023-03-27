fb1208be5eb7c847be7e8f5dd05fa3d8ba26b73f
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-28 00:41:37 +0600

    products card dynamic info added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -6,7 +6,13 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>{name}</h6>
+            
+            <div className="product-info">
+            <h6 className='product-name'>{name}</h6>
+            <p>Price: ${price}</p>
+            <p>Manufacturer: {seller}</p>
+            <p>Ratings: {ratings} stars</p>
+            </div>
         </div>
     );
 };
