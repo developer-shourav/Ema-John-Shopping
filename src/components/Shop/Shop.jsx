233e806ab435b43bdfab363b8d2393caf15d3e30
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-27 23:55:40 +0600

    products data fetching complete

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect( () => {
+       fetch('fakeData/products.json')
+       .then( res => res.json())
+       .then( data => setProducts(data))
+    },  [])
     return (
         <div className='shop-container'>
             <div className="products-container">
-             <h2>Product is coming here.</h2>
+             <h2>Product is coming here: {products.length}</h2>
             </div>
 
             <div className="cart-container">
