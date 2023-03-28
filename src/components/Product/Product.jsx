7babbd8ce54d0cb4ee4adc42e424a3d3a70aee73
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-28 22:39:46 +0600

    component added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,18 +3,24 @@ import './Product.css'
 
 const Product = (props) => {
     const {id, img, name, price, quantity, ratings, seller, stock} = props?.product;
+
+    const handleAddToCart = () => {
+        console.log('Product added');
+    }
     return (
-        <div className='product'>
-            <img src={img} alt="" />
-            
-            <div className="product-info">
-            <h6 className='product-name'>{name}</h6>
-            <p>Price: ${price}</p>
-            <p>Manufacturer: {seller}</p>
-            <p>Ratings: {ratings} stars</p>
-            </div>
-            <button className='btn-cart'>Add to Cart</button>
+      <div className="product">
+        <img src={img} alt="" />
+
+        <div className="product-info">
+          <h6 className="product-name">{name}</h6>
+          <p>Price: ${price}</p>
+          <p>Manufacturer: {seller}</p>
+          <p>Ratings: {ratings} stars</p>
         </div>
+        <button onClick={() => handleAddToCart()} className="btn-cart">
+          Add to Cart
+        </button>
+      </div>
     );
 };
 
