824555272b8f323a2291f8534ba35a0c7ef1dbc4
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-29 21:44:17 +0600

    product calculation added anad cart design complete

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,18 +3,22 @@ import "./Cart.css";
 
 const Cart = ({cart}) => {
    
-    let total = 0 ;
+    let totalPrice = 0 ;
+    let totalShipping = 0;
     for( const product of cart){
-        total = total + product.price ;
+        totalPrice = totalPrice + product.price ;
+        totalShipping = totalShipping + product.shipping ;
     }
+    const tax = (totalPrice*7) / 100;
+    const grandTotal = totalPrice + totalShipping + tax ;
   return (
     <div className="cart">
       <h3>Order Summery </h3>
       <p>Selected Items :{cart.length}</p>
-      <p>Total Price: ${total}</p>
-      <p>Total Shipping Charge: $</p>
-      <p>Tax: $</p>
-      <h4>Grand Total: $1559</h4>
+      <p>Total Price: ${totalPrice.toFixed(2)}</p>
+      <p>Total Shipping Charge: ${totalShipping.toFixed(2)}</p>
+      <p>Tax(7%): ${tax.toFixed(2)}</p>
+      <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
     </div>
   );
 };
