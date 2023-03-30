93f25343d328148a19dbbab9434e891281306b7e
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-30 07:16:28 +0600

    local storage data getting method added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -20,6 +20,11 @@ const Shop = () => {
        .then( res => res.json())
        .then( data => setProducts(data))
     },  []);
+
+    useEffect( () => {
+        const storedCart = getShoppingCart();
+        console.log(storedCart);
+    }, [])
 
     return (
         <div className='shop-container'>
