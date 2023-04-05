bbb19b4547d94816db962e69a67c9274b1adcd56
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-06 01:31:17 +0600

    loaders added

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Cart from '../Cart/Cart';
+import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
+    const products = useLoaderData();
+    console.log(products);
     return (
         <div className='shop-container'>
             <div className='products-container'>
--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,8 @@ const router = createBrowserRouter([
       },
       {
         path:'orders',
-        element:<Orders> </Orders>
+        element:<Orders> </Orders>,
+        loader : () => fetch('products.json'),
       },
       {
         path:'inventory',
