25e6d84cdc8ed069d200ed86758f596750944261
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-06 01:19:44 +0600

    orders added

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Cart from '../Cart/Cart';
 
 const Orders = () => {
     return (
-        <div>
-            <h2>Orders Page</h2>
+        <div className='shop-container'>
+            <div className='products-container'>
+                <h2>Order Page</h2>
+            </div>
+
+            <div className='cart-container'>  
+                <Cart cart={[]}> </Cart>
+            </div>
+            
         </div>
     );
 };
