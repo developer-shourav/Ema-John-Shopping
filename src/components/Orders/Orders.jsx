950b53c5a350d294573ead5f95ca7e63483a18a3
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-06 02:03:25 +0600

    returnng method added

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Cart from '../Cart/Cart';
+import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
+    const products = useLoaderData();
+    console.log(products);
     return (
         <div className='shop-container'>
             <div className='products-container'>
-                <h2>Order Page: {products?.length}</h2>
+                <h2>Order Page:{products.length}</h2>
             </div>
 
             <div className='cart-container'>  
--- src/loaders/cartProductsLoaders.js
+++ src/loaders/cartProductsLoaders.js
@@ -1,8 +1,29 @@
+import { getShoppingCart } from "../utilities/fakedb";
 
 const cartProductsLoader = async () => {
     const loadedProducts = await fetch('products.json');
     const products = await loadedProducts.json();
-    console.log(products);
+  
+    // If cart Data is in Database , I have to use Async await
+    const storedCart = getShoppingCart();
+
+    const savedCart = [];
+    for( const id in storedCart){
+        const addedProduct = products.find(pd => pd?.id == id);
+        if( addedProduct){
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+
+        }
+    }
+
+    // If I need to return two this form a function : The method is
+    // 1. Return as an array [itemOne, itemTwo];
+    // 2. Return as an object { itemOne, itemTow}
+
+
+    return savedCart;
 }
 
 export default cartProductsLoader;
