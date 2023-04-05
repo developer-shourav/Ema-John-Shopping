f3577f3f06706f66402e2004892e5c5abb51cf94
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-05 23:30:50 +0600

    first route added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
-                <a href="/shop">Shop</a>
+                <a href="/">Shop</a>
                 <a href="/order">Orders</a>
                 <a href="/inventory">Inventory</a>
                 <a href="/login">Login</a>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import App from './App';
+import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from './components/Shop/Shop';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Shop> </Shop>
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
