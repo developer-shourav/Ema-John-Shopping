d1c90b2eca3b4046b60f588b72aefea4cc88fa9b
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-05 23:59:23 +0600

    element added

--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -4,6 +4,9 @@ const Inventory = () => {
     return (
         <div>
             <h2>This is inventory</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt libero eveniet dolore aperiam adipisci aliquam sunt laudantium iste unde nulla laboriosam eius dolorem impedit quod, doloremque incidunt ex tempora fugit earum omnis. Recusandae quisquam unde nihil. A cumque modi incidunt dolorem minima et adipisci ipsa asperiores odit. Mollitia unde eum, esse voluptates officiis, quaerat, at neque est obcaecati odit non? Odit perspiciatis modi tenetur, enim facere veritatis ipsam aliquam ut repudiandae quisquam eos suscipit dolores dicta repellat error quo officiis eveniet ducimus! Hic voluptatibus sunt molestias voluptates, iusto odit similique est porro, itaque quo quam. Magni necessitatibus provident amet molestiae libero repudiandae ab itaque quos nobis? Porro incidunt itaque dolor iusto, laudantium consectetur officiis, nostrum nisi eum possimus atque cumque, ducimus pariatur quod delectus. Aperiam ea, distinctio, commodi consequuntur labore maxime facilis molestias nisi placeat, odio nam provident quidem voluptatem iste. Cumque, praesentium accusamus mollitia ipsa, officiis ratione tempore illum omnis, est dicta velit necessitatibus minus ea eos quia cupiditate harum voluptas. Porro illum doloribus excepturi rerum quidem a molestias. Inventore maiores aut mollitia iure, tempore odit dicta excepturi, dolor, aliquam harum sit iste? Voluptatum aperiam temporibus doloremque ad dolorem ullam accusamus, distinctio, amet impedit quasi possimus, earum magni itaque?
+            </p>
         </div>
     );
 };
