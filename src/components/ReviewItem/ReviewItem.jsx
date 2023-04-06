4b32ec0102e6c565155c9a2fce6e43d83c01cd53
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-06 10:40:41 +0600

    reviewItem container added

---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ReviewItem.css';
+const ReviewItem = ({product}) => {
+    return (
+        <div>
+            <h2>Review Items</h2>
+        </div>
+    );
+};
+
+export default ReviewItem;
