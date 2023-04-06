888f4927f03f6eab05188bb17885c7b23fa1ce51
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-06 21:48:42 +0600

    clear cart btn added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Cart.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Cart = ({ cart }) => {
     // const cart = props.cart; // option 1
@@ -32,6 +34,8 @@ const Cart = ({ cart }) => {
             <p>Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
             <h6>Grand Total: ${grandTotal.toFixed(2)} </h6>
+
+            <button className='btn-clear-cart'> <span>Clear Cart</span> <FontAwesomeIcon  icon={faTrashAlt} /> </button>
         </div>
     );
 };
