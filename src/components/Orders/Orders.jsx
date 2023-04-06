5518ace3ec511d2160323b53436f80770eb67fa4
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-06 21:31:03 +0600

    order delete form card added

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
-import { useLoaderData } from 'react-router-dom';
+import { json, useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css';
+import { removeFromDb } from '../../utilities/fakedb';
 const Orders = () => {
     const savedCart = useLoaderData();
     const [cart, setCart] = useState(savedCart);
 
     const handleRemoveFormCart = (id) => {
-        console.log(id);
-        savedCart.map
+        
+        const remaining = cart?.filter( product => product?.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+       
     }
     return (
         <div className='shop-container'>
